fbbceac9304d5fd26bfa9ed113e2c0448a6456a8
Jo Blaszak <jblaszak@gmail.com>
2022-09-23 12:42:33 -0700

    Moved deploy file and updated repository base.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jblaszak.github.io",
-  base: "practice-blogr",
+  base: "/practice-blogr",
   integrations: [react()],
 });
